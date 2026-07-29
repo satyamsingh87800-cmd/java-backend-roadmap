820d57109b01103028b71a29543999bd6d30e12d
Satyamsinghhhh <satyamsingh95863@gmail.com>
2026-07-29 18:32:40 +0530

    now its all clear about scanner function
    km to miles converter

---
+++ exercise1_5.java
@@ -0,0 +1,28 @@
+//km to miles converter 
+import java.util.Scanner;
+public class exercise1_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter your Kilo meter value");
+        int kilo = sc.nextInt();
+double b =  0.621371;
+        double total = kilo*b;
+
+        System.out.println("YOUR KILOMETER VALUE IS: " + kilo );
+        System.out.println("YOUR MILES VALUE IS: " + total);
+sc.close();
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+// 0.621371 * km 
