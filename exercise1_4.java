b700d96c9caa0aa557b4313ec2614a78d40c1935
Satyamsinghhhh <satyamsingh95863@gmail.com>
2026-07-29 18:31:49 +0530

    take a input from user to greet them
    this helps me to understand the scanner fuction much more hence i have lots of problem init.

---
+++ exercise1_4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class exercise1_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name ");
+        String name = sc.next();
+        System.out.println("hello " + name + " have a nice day");
+        sc.close();
+
+    }
+}
