a742d7f3d6c7bf3b93be56fc1163068296585ccb
Satyamsinghhhh <satyamsingh95863@gmail.com>
2026-08-01 12:04:34 +0530

    encrypt and decrypt
    encrypt and decrypt grade so no one can uderstand your result .
    one more we will use typecasting in this exercise

---
+++ exercise2_4.java
@@ -0,0 +1,10 @@
+public class exercise2_4 {
+    public static void main(String[] args) {
+        int a = 15;
+        int v = 48;
+        int u = 22;
+        int s = 56;
+        double formula = (v * v - u * u) / 2 * a * s;
+        System.out.println(formula);
+    }
+}
---
+++ exercise2_5.java
@@ -0,0 +1,9 @@
+public class exercise2_5 {
+    public static void main(String[] args) {
+        int x = 7;
+        int a = x*49/7 + 35/x;
+        System.out.println(a);
+    }
+    
+   //find the value of a  
+}
