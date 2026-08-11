42b76305a3946d8b5cef6a6d092869eb13c591b9
Satyamsinghhhh <satyamsingh87800@gmail.com>
2026-08-11 19:11:36 +0530

    Create exercise3_1.java

---
+++ exercise3_1.java
@@ -0,0 +1,24 @@
+public class exercise3_1 {
+    public static void main(String[] args) {
+        String name = "Satyam";
+        // System.out.println(name.toUpperCase()); convert string into upper case.
+        // System.out.println(name.toLowerCase()); convert string into lower case.
+        // System.out.println(name.length()); tell how many element in there.
+        // System.out.println(name.trim()); remove all the white space between the
+        // srting.
+        // System.out.println(name.substring(3)); it return a new substring from start
+        // to end
+        // System.out.println(name.substring(2,4)); same as above but have start and end
+        // point if its have 4 it will end the string at 3 .
+        // System.out.println(name.replace('a','f'));
+        // System.out.println(name.startsWith("ty"));
+        // System.out.println(name.endsWith("am"));
+        // System.out.println(name.charAt(2));
+        // System.out.println(name.indexOf('m'));
+           //System.out.println(name.lastIndexOf('t'));
+                      //System.out.println(name.equals("ty"));
+
+
+    }
+
+}
