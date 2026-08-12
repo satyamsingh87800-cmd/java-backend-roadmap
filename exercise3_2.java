2e031ebece974d1ff39e6e75b1361e770901fd78
Satyamsinghhhh <satyamsingh87800@gmail.com>
2026-08-12 10:58:03 +0530

    convert string in lower case
    toLowerCase()

---
+++ exercise3_2.java
@@ -0,0 +1,8 @@
+public class exercise3_2 {
+    public static void main(String[] args) {
+        String let = "Styam";
+        //convert string in lower case
+        let= let.toLowerCase();
+        System.out.println(let);
+    }
+}
