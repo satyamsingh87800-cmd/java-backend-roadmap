a7873636375c5b23261ce39860ba1fd00aeedd5b
Satyamsinghhhh <satyamsingh87800@gmail.com>
2026-08-12 10:54:08 +0530

    some notes

--- exercise3_1.java
+++ exercise3_1.java
@@ -10,15 +10,13 @@ public class exercise3_1 {
         // to end
         // System.out.println(name.substring(2,4)); same as above but have start and end
         // point if its have 4 it will end the string at 3 .
-        // System.out.println(name.replace('a','f'));
-        // System.out.println(name.startsWith("ty"));
-        // System.out.println(name.endsWith("am"));
-        // System.out.println(name.charAt(2));
-        // System.out.println(name.indexOf('m'));
-        // System.out.println(name.lastIndexOf('t'));
-        // System.out.println(name.equals("ty"));
-        System.out.println(name.startsWith("ty"));
+        // System.out.println(name.replace('a','f')); it return anew string after replacing the letter a with f
 
+        // System.out.println(name.startsWith("ty")); its a bool type it return true and false if it starts with ty 
+        // System.out.println(name.endsWith("am"));  its a bool type it return true and false if it end  with ty 
+        // System.out.println(name.charAt(2));    it gives a element at that location 
+        // System.out.println(name.indexOf('m'));   it return the index number of m
+        
     }
 
 }
