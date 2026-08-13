a18ad61a39498db53bd44658c1ddd034cda82ad1
Satyamsinghhhh <satyamsingh87800@gmail.com>
2026-08-13 16:28:28 +0530

    Create exercise3_5.java

---
+++ exercise3_5.java
@@ -0,0 +1,7 @@
+public class exercise3_5 {
+    public static void main(String[] args) {
+                String letter = "Dear name,     Thanks alot";
+ System.out.println(letter.indexOf("  ")) ;
+ System.out.println(letter.indexOf("    ")) ;
+    }
+}
