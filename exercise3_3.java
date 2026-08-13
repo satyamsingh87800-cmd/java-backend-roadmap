60d1e2fccee0299e0fdf60976a5f2f9a274bf26a
Satyamsinghhhh <satyamsingh87800@gmail.com>
2026-08-13 15:58:45 +0530

    Create exercise3_3.java

---
+++ exercise3_3.java
@@ -0,0 +1,9 @@
+public class exercise3_3 {
+    public static void main(String[] args) {
+        String letter = "my name is sudo";
+        letter= letter.replace(' ','_');
+        System.out.println(letter);
+        //replace space with _ 
+        //my_name_is_sudo
+    }
+}
