65e7d11f2e2e2e370be2d45d62d6b5a770be5eff
Satyamsinghhhh <satyamsingh87800@gmail.com>
2026-08-13 16:28:17 +0530

    Create exercise3_4.java

---
+++ exercise3_4.java
@@ -0,0 +1,8 @@
+public class exercise3_4 {
+    public static void main(String[] args) {
+        String letter = "Dear <name>, Thanks alot";
+        letter = letter.replace("<name>","satyam" );
+        System.out.println(letter);
+    }
+}
+//
