8d8a63647bbe98aaeb98176a6c79fe7f3dfbdd76
Satyamsinghhhh <satyamsingh87800@gmail.com>
2026-08-14 10:55:07 +0530

    Create exercise4_3.java

---
+++ exercise4_1.java
@@ -0,0 +1,12 @@
+public class exercise4_1 {
+    public static void main(String[] args) {
+        int a =7;
+        if (a == 12 )
+        {
+            System.out.println("i am 12");
+        }
+        else {
+            System.out.println("i am not 12");
+        }
+    }
+}
