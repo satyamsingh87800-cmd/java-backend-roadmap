31af49de5ecc5f4f9355d0f657b6958ff6095f03
Satyamsinghhhh <satyamsingh87800@gmail.com>
2026-08-15 16:12:58 +0530

    Update exercise4_3.java

--- exercise4_3.java
+++ exercise4_3.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class exercise4_3 {
    public static void main(String[] args) {
-    //now we will see this with the switch case 
+      Scanner sc = new Scanner(System.in);
+      System.out.println("enter your salary");
+      float salary = sc.nextFloat();
+      if (salary < 250000) {
+         System.out.println("no tax for you. \n but need to do better");
+      }
+      else if (salary > 1000000 )
+   sc.close();
    } 
 }
